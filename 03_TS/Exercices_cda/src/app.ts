96a6c1b9c6211bf116686641e14c9ceb4ac3c64d
Antoine07 <antoine.lucsko@gmail.com>
2024-12-10 13:56:23 +0100

    exo container

--- 03_TS/Exercices_cda/src/app.ts
+++ 03_TS/Exercices_cda/src/app.ts
@@ -1,40 +1,56 @@
 
-let a : number = 1
+import { addition } from "./types/Person"
+
+let a: number = 1
 
 console.log('hello', a)
 
-let b : number = 1
+let b: number = 1
 
 b = 100
 
-function add(a :number, b : number): number{
+function add(a: number, b: number): number {
 
     return a + b
 }
 
 
-add (1, add(2,2))
+add(1, add(2, 2))
 
 let nombres: number[] = [1, 2, 3, 4];
 
 type Address = string;
 
 interface Student {
-    name : string;
-    address : Address;
-    notes? : number[];
+    name: string;
+    address: Address;
+    notes?: number[];
 }
 
 
-const u : Student = {
-    name : "Alan",
-    address : "Paris",
-    notes : [11, 13, 15, 19],
+const u: Student = {
+    name: "Alan",
+    address: "Paris",
+    notes: [11, 13, 15, 19],
 }
 
 // typage générique nous allons le voir plus loing
 type Pair<T> = [T, T];
 
-const p : Pair<string> = ["a", "b"]
+// tu fixes le type T = string
+const p: Pair<string> = ["a", "b"]
 
-const q : Pair<number[]> = [[1,2], [3,4]]
+const q: Pair<number[]> = [[1, 2], [3, 4]]
+
+
+// Add peut maintenant être défini avec le type addition
+const Add: addition = (a, b) => {
+
+    return a + b
+}
+
+// dans les cheffrons sont définis les types génériques
+function shuffleElement<T, G>( n : T, m : G) : T {
+
+    return n
+}
---
+++ 03_TS/Exercices_cda/src/types/ErrorPerson.ts
@@ -0,0 +1,4 @@
+export type ErrorPerson = {
+    code: number;
+    message: string;
+}
